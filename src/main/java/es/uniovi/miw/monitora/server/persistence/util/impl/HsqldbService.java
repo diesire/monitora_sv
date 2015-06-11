703fe48f5c39b507cfb81b7ad9909c3132327adc
Pablo Escalada <diesire@gmail.com>
2015-06-11 15:00:50 +0200

    Add configuration support

--- src/main/java/es/uniovi/miw/monitora/server/persistence/util/impl/HsqldbService.java
+++ src/main/java/es/uniovi/miw/monitora/server/persistence/util/impl/HsqldbService.java
@@ -3,28 +3,26 @@ package es.uniovi.miw.monitora.server.persistence.util.impl;
 import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.text.MessageFormat;
 
 import org.hsqldb.Server;
 import org.hsqldb.persist.HsqlProperties;
 import org.hsqldb.server.ServerAcl.AclFormatException;
 
+import es.uniovi.miw.monitora.server.conf.Conf;
 import es.uniovi.miw.monitora.server.model.exceptions.BusinessException;
 import es.uniovi.miw.monitora.server.persistence.util.PersistenceService;
 
 public class HsqldbService implements PersistenceService {
-	private static final String MONITORASV = "monitorasv";
-	private static final String RELATIVE = "file:./data/serverdb";
-	private static final String FULL = "file:/Users/diesire/Documents/workspace/monitora_sv/data/serverdb";
-	private static final String TEST = FULL;
 	private Server server2;
 
 	public void start() throws BusinessException {
 		HsqlProperties p = new HsqlProperties();
 		server2 = new Server();
 
-		p.setProperty("server.database.0", TEST);
-		p.setProperty("server.dbname.0", MONITORASV);
-		p.setProperty("server.port", "9001");
+		p.setProperty("server.database.0", Conf.get("database"));
+		p.setProperty("server.dbname.0", Conf.get("dbname"));
+		p.setProperty("server.port", Conf.get("port"));
 		p.setProperty("readonly", "true");
 
 		try {
@@ -34,8 +32,11 @@ public class HsqldbService implements PersistenceService {
 			server2.start();
 
 			DriverManager.registerDriver(new org.hsqldb.jdbcDriver());
-			DriverManager.getConnection("jdbc:hsqldb:hsql://localhost:9001/"
-					+ MONITORASV + ";shutdown=true;ifexists=true", "sa", "");
+			DriverManager.getConnection(
+							MessageFormat
+									.format("jdbc:hsqldb:hsql://localhost:{0}/{1};shutdown=true;ifexists=true",
+											Conf.get("port"),
+											Conf.get("dbname")), "sa", "");
 
 			// Statement stat = c.createStatement();
 			// Boolean result = stat
