dafe8d1dcacb0efe5e37c65d2608a779346fb1cd
Pablo Escalada <diesire@gmail.com>
2015-06-23 02:44:51 +0200

    Add  fromXML

--- src/main/java/es/uniovi/miw/monitora/server/persistence/util/PersistenceService.java
+++ src/main/java/es/uniovi/miw/monitora/server/persistence/util/PersistenceService.java
@@ -1,5 +1,6 @@
 package es.uniovi.miw.monitora.server.persistence.util;
 
+import java.nio.file.Path;
 import java.sql.Connection;
 
 import es.uniovi.miw.monitora.server.model.exceptions.BusinessException;
@@ -8,4 +9,6 @@ public interface PersistenceService {
 	void start() throws BusinessException;
 	void stop() throws BusinessException;
 	Connection getConnection() throws BusinessException;
+	void dumpToXML(String tabla, Path tempFile) throws BusinessException;
+	void fromXML(Path tempFile) throws BusinessException;
 }
--- src/main/java/es/uniovi/miw/monitora/server/persistence/util/impl/HsqldbService.java
+++ src/main/java/es/uniovi/miw/monitora/server/persistence/util/impl/HsqldbService.java
@@ -1,14 +1,29 @@
 package es.uniovi.miw.monitora.server.persistence.util.impl;
 
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.text.MessageFormat;
 
+import javax.sql.rowset.WebRowSet;
+
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.database.QueryDataSet;
+import org.dbunit.dataset.NoSuchTableException;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
 import org.hsqldb.Server;
 import org.hsqldb.persist.HsqlProperties;
 import org.hsqldb.server.ServerAcl.AclFormatException;
+
+import com.sun.rowset.WebRowSetImpl;
 
 import es.uniovi.miw.monitora.server.conf.Conf;
 import es.uniovi.miw.monitora.server.model.exceptions.BusinessException;
@@ -49,19 +64,17 @@ public class HsqldbService implements PersistenceService {
 		// TODO: change localhost to Conf.get
 		Connection connection;
 		try {
-			connection = DriverManager
-					.getConnection(
-							MessageFormat
-									.format("jdbc:hsqldb:hsql://localhost:{0}/{1};shutdown=true;ifexists=true",
-											Conf.get("db.port"),
-											Conf.get("db.dbname")), "sa", "");
+			connection = DriverManager.getConnection(getURL(), "sa", "");
 		} catch (SQLException e) {
 			throw new BusinessException(e);
 		}
 		return connection;
-		// Statement stat = c.createStatement();
-		// Boolean result = stat
-		// .execute("CREATE TABLE Test( Id INTEGER PRIMARY KEY, FirstName VARCHAR(20), Name VARCHAR(50), ZIP INTEGER)");
+	}
+
+	private String getURL() {
+		return MessageFormat
+				.format("jdbc:hsqldb:hsql://localhost:{0}/{1};shutdown=true;ifexists=true",
+						Conf.get("db.port"), Conf.get("db.dbname"));
 	}
 
 	public void stop() throws BusinessException {
@@ -73,5 +86,76 @@ public class HsqldbService implements PersistenceService {
 			instance = new HsqldbService();
 		}
 		return instance;
+	}
+
+	public synchronized void dumpToXML(String tabla, Path tempFile)
+			throws BusinessException {
+		// try {
+		// WebRowSet webRS = new WebRowSetImpl();
+		// webRS.setCommand("SELECT * FROM " + tabla);
+		// webRS.execute(getConnection());
+		// FileWriter fw = new FileWriter(tempFile.toFile());
+		// webRS.writeXml(fw);
+		// fw.flush();
+		// fw.close();
+		// webRS.close();
+		//
+		// } catch (SQLException | IOException e) {
+		// throw new BusinessException(e);
+		// }
+		// database connection
+
+		try {
+			IDatabaseConnection connection = new DatabaseConnection(
+					getConnection());
+
+			// partial database export
+			QueryDataSet partialDataSet = new QueryDataSet(connection);
+			partialDataSet.addTable(tabla);
+			FlatXmlDataSet.write(partialDataSet,
+					new FileOutputStream(tempFile.toFile()));
+		} catch (Exception e) {
+			throw new BusinessException(e);
+		}
+	}
+
+	public synchronized void fromXML(Path tempFile) throws BusinessException {
+		// //
+		// https://docs.oracle.com/javase/tutorial/jdbc/basics/cachedrowset.html#updating-data-source
+		// // https://docs.oracle.com/javase/tutorial/jdbc/basics/webrowset.html
+		// try {
+		// FileReader fr = new FileReader(tempFile.toFile());
+		// WebRowSet webRs1 = new WebRowSetImpl();
+		// webRs1.readXml(fr);
+		// while (webRs1.next()) {
+		// webRs1.getR
+		// }
+		//
+		// WebRowSet webRs2 = new WebRowSetImpl();
+		// String url = getURL();
+		// webRs2.setUrl(url);
+		// webRs2.setUsername("sa");
+		// webRs2.setPassword("");
+		//
+		// webRs2.acceptChanges();
+		// // webRs.acceptChanges(getConnection());
+		// fr.close();
+		// webRs2.close();
+		// } catch (SQLException | IOException e) {
+		// throw new BusinessException(e);
+		// }
+		try {
+			FlatXmlDataSet dataSet = new FlatXmlDataSetBuilder().build(tempFile
+					.toFile());
+			IDatabaseConnection connection = new DatabaseConnection(
+					getConnection());
+			try {
+				DatabaseOperation.REFRESH.execute(connection, dataSet);
+			} catch (NoSuchTableException e) {
+				DatabaseOperation.INSERT.execute(connection, dataSet);
+			}
+		} catch (Exception e) {
+			throw new BusinessException(e);
+		}
 	}
 }
